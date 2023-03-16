ba126193e077744a975047fd0fd7e60151a07634
mborreros <mlbsindia@gmail.com>
2023-03-16 12:12:07 -0400

    updated sample product names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-puts "Creating sample shelves"
+puts "Creating warehouse shelves"
 
 (1..49).each { |num| Shelf.create!(name: num.to_s + "A")}
 (1..49).each { |num| Shelf.create!(name: num.to_s + "B")}
@@ -6,12 +6,14 @@ puts "Creating sample shelves"
 (1..49).each { |num| Shelf.create!(name: num.to_s + "D")}
 (1..49).each { |num| Shelf.create!(name: num.to_s + "E")}
 
-puts "Finished creating sample shelves"
+puts "Finished creating warehouse shelves"
 
 puts "Creating sample product records"
-Product.create!(name: "apple", lot_number: "45FG7", weight: 18, shelf_id: 3, sap_material_number: "204419")
-Product.create!(name: "banana", lot_number: "7UT90", weight: 6, shelf_id: 2, sap_material_number: "304219")
-Product.create!(name: "clementine", lot_number: "P21DF", weight: 1, shelf_id: 2, sap_material_number: "144419")
-Product.create!(name: "durian", lot_number: "754RT", weight: 24, shelf_id: 1, sap_material_number: "274419")
-Product.create!(name: "elote", lot_number: "LU45G", weight: 12, shelf_id: 3, sap_material_number: "154419")
+
+Product.create!(name: "sample_product_1", lot_number: "45FG7", weight: 18, shelf_id: 3, sap_material_number: "204419")
+Product.create!(name: "sample_product_2", lot_number: "7UT90", weight: 6, shelf_id: 2, sap_material_number: "304219")
+Product.create!(name: "sample_product_3", lot_number: "P21DF", weight: 1, shelf_id: 2, sap_material_number: "144419")
+Product.create!(name: "sample_product_4", lot_number: "754RT", weight: 24, shelf_id: 1, sap_material_number: "274419")
+Product.create!(name: "sample_product_5", lot_number: "LU45G", weight: 12, shelf_id: 3, sap_material_number: "154419")
+
 puts "Finished creating sample product records"
